2b8c1034536996d5d4d6d9bfe4bc157882d3ce58
Delta Ink Design <izula97@gmail.com>
2019-02-21 07:30:41 +0100

    Create Fibonacci.asm

---
+++ Fibonacci.asm
@@ -0,0 +1,18 @@
+﻿.text
+.globl _start
+_start:
+    mov $1, %EAX
+    mov $1, %EBX
+    mov $5, %ECX
+while:
+    cmp $1,%ECX
+    jle end_while
+    add %EAX, %EBX
+    xchg %EAX, %EBX
+    dec %ECX
+    jmp while
+end_while:
+    nop
+    mov $0, %EBX
+    mov $1, %EAX
+    int $0x80
