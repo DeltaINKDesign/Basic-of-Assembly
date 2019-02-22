dab677cab0e0eae8eee66938d492ee13e793351d
Delta Ink Design <izula97@gmail.com>
2019-02-22 10:12:09 +0100

    Create Euklides.asm

---
+++ Euklides.asm
@@ -0,0 +1,19 @@
+﻿.text
+.globl _start
+_start:
+    mov $14, %EAX
+    mov $21, %EBX
+while:
+    cmp %EAX, %EBX
+    jz end_while
+    jg else
+    sub %EBX, %EAX
+    jmp end_if
+else:
+    sub %EAX, %EBX
+end_if:
+    jmp while
+end_while:
+    mov $0, %EBX
+    mov $1, %EAX
+    int $0x80
