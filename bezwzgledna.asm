d6963a57382133181df68afdbf88835923b183d1
Delta Ink Design <izula97@gmail.com>
2019-02-25 23:25:00 +0100

    Create bezwzgledna.asm

---
+++ bezwzgledna.asm
@@ -0,0 +1,12 @@
+﻿
+.globl _start
+.text
+_start:
+    mov $4, %ECX
+    mov $2, %EDX
+    sub %ECX, %EDX   #sub ustawia flagi wiec juz nie trzeba cmp
+    jg godown
+    neg %EDX
+    godown:
+    mov %EDX, %EBX
+    nop
