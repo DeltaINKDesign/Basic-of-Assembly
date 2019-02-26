cd1911ce2d204a6e90e685fdbb87edeff7175eb8
Delta Ink Design <izula97@gmail.com>
2019-02-26 11:54:51 +0100

    Create max3rejestry.asm

---
+++ max3rejestry.asm
@@ -0,0 +1,15 @@
+﻿
+.globl _start
+.text
+_start:
+    mov $4, %ECX
+    mov $2, %EDX
+    mov  $5, %EBX
+    mov %ECX, %EAX
+    cmp %EDX, %EAX
+    jg end_if
+    mov %EBX, %EAX
+    jg end_if
+end_if:
+    cmp %
+    nop
