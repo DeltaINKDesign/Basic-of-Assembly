eed0efc82a1a2b23e3b04dd391e7d8cba9eacfc3
DeltaINKDesign <izula97@gmail.com>
2019-02-28 12:38:04 +0100

    Create kwadratindeksu.asm

---
+++ kwadratindeksu.asm
@@ -0,0 +1,19 @@
+﻿.equ max, 40
+.bss
+tab: .space max
+etab:
+.text
+.globl _start
+.text
+_start:
+    mov $0, %EBX
+while:
+    cmp $(etab-tab)/4,%EBX
+    jge endwhile
+    mov %EBX, %EAX
+    mul %EAX
+    mov %EAX, tab(,%EBX,4)
+    inc %EBX
+    jmp while
+endwhile:
+    nop
