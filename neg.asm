af7faca44da1de666f174631cf87b3de6c763904
DeltaINKDesign <izula97@gmail.com>
2019-03-01 10:08:48 +0100

    Create neg.asm

---
+++ neg.asm
@@ -0,0 +1,15 @@
+﻿
+.globl _start
+.text
+_start:
+        mov $4, %EBX
+        mov $3, %ECX
+        mov $1, %EAX
+while:
+        cmp $0, %EBX
+        je end_while
+        mul %EBX
+        dec %EBX
+        jmp while
+end_while:
+        nop
