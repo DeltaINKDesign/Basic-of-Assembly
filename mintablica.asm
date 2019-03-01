0f5aa746100a520164c1dd63c1b62a0a26c8fe0f
DeltaINKDesign <izula97@gmail.com>
2019-03-01 10:09:10 +0100

    Create mintablica.asm

---
+++ mintablica.asm
@@ -0,0 +1,21 @@
+﻿.data
+tab: .long 3,6,7,2,4,1
+end_tab:
+.equ tab_bytes, end_tab-tab
+
+.globl _start
+.text
+_start:
+    mov tab, %EAX
+    mov $4, %EBX
+while:
+    cmp $tab_bytes, %EBX
+    jge end_while
+    cmp tab(%EBX), %EAX
+    jle end_if
+    mov tab(%EBX),%EAX
+end_if:
+    add $4, %EBX
+    jmp while
+end_while:
+    nop
