4a235295d73108f7e6605be6d0e914eeaeaaddc4
DeltaINKDesign <izula97@gmail.com>
2019-03-02 10:34:22 +0100

    Create nwd.asm

---
+++ nwd.asm
@@ -0,0 +1,22 @@
+﻿
+.globl _start
+.text
+_start:
+nwd:
+while:
+        cmp %EAX, %EBX
+        jz end_while
+        jg else
+        sub %EBX, %EAX
+        jmp while
+else:
+        sub %EAX,%EBX
+end_if:
+        jmp while
+end_while:
+        ret
+        _start:
+        mov $14,%EAX
+        mov $28,%EBX
+        call nwd
+        nop
